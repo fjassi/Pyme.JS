4ca12b8caa2d5d1073f0a8f02e242848cb7f8890
Marcos Iglesias <markkos89@gmail.com>
2016-11-03 19:51:24 -0300

    ABM Clientes

--- controllers/cClientes.js
+++ controllers/cClientes.js
@@ -3,6 +3,8 @@ const  mCdiva = require('../models/mCdiva');
 const mTidofis = require('../models/mTidofis');
 const mProvincias = require('../models/mProvincias');
 
+var tools = require('../public/js/utils.js');
+
 module.exports = {
 	getLista: getLista,
 	getAlta: getAlta,
@@ -43,6 +45,28 @@ function sp_clientes(req, res){
 	var oCliente = req.body;
 	console.log(oCliente)
 
+	if (oCliente.posee == 'on')
+		oCliente.posee = 'S';
+	else
+		oCliente.posee = 'N';
+
+	if (oCliente.habilitada == 'on')
+		oCliente.habilitada = 'S';
+	else
+		oCliente.habilitada = 'N';
+
+	if (oCliente.saldopositivo == 'on')
+		oCliente.saldopositivo = 'S';
+	else
+		oCliente.saldopositivo = 'N';
+
+	if (oCliente.baja == 'on')
+		oCliente.baja = 'S';
+	else
+		oCliente.baja = 'N';
+
+	if(oCliente.fecha_alta.length > 0)
+		oCliente.fecha_alta = tools.changeDate(oCliente.fecha_alta);
 	
 	mClientes.sp_clientes(oCliente, function(){
 		res.redirect('/clientes/lista');
@@ -51,22 +75,31 @@ function sp_clientes(req, res){
 
 function getModificar(req, res){
 	const params = req.params;
-	const id = params.id;
+	const numero = params.numero;
 
-	mClientes.getById(id, function(cliente){
-		res.render("clientes_modificar", {
-			pagename: "Modificar Cliente",
-			cliente: cliente[0]
+	mCdiva.getAll(function(codigos_iva){
+		mTidofis.getAll(function(tipo_docs){
+			mProvincias.getAll(function(provincias){
+				mClientes.getByNumero(numero, function(cliente){
+					res.render("clientes_modificar", {
+						pagename: "Modificar Cliente",
+						cliente: cliente[0],
+						codigos_iva: codigos_iva,
+						tipo_docs: tipo_docs,
+						provincias: provincias
+					});
+				});
+			});
 		});
-	});	
+	});		
 }
 
 function getEliminar(req, res){
 	const params = req.params;
-	const id = params.id;
+	const numero = params.numero;
 
 	// verificar movimientos
-	mClientes.del(id, function(){
+	mClientes.del(numero, function(){
 		res.redirect('/clientes/lista');
 	});
 }
--- models/mClientes.js
+++ models/mClientes.js
@@ -2,7 +2,7 @@ var conn = require('../config/db').conn;
 
 module.exports = {
 	getAll: getAll,
-	getByCodigo: getByCodigo,
+	getByNumero: getByNumero,
 	sp_clientes: sp_clientes,
 	del: del,
 	getNextNumero: getNextNumero,
@@ -10,15 +10,40 @@ module.exports = {
 }
 
 function getAll(cb){
-	conn("select * from clientes order by cl_nume", cb);
+	conn("select *, "+
+		"CASE WHEN clientes.cl_ctac = 'S' THEN 'Si' WHEN clientes.cl_ctac = 'N' THEN 'No' END as cuentacorrientatxt, "+
+		"CASE WHEN clientes.cl_habi = 'S' THEN 'Si' WHEN clientes.cl_habi = 'N' THEN 'No' END as habilitadotxt "+
+		"from clientes order by cl_nume", cb);
 }
 
-function getByCodigo(numero, cb){
-	conn("select * from clientes where cl_nume = "+numero, cb);
+function getByNumero(numero, cb){
+	conn("select *, RTRIM(LTRIM(cl_prov)) as provinciatxt, "+
+		"RTRIM(LTRIM(cl_apel)) as razonsocialtxt, "+
+		"RTRIM(LTRIM(cl_direc)) as direcciontxt, "+
+		"RTRIM(LTRIM(cl_local)) as localidadtxt, "+
+		"RTRIM(LTRIM(cl_cp)) as codigopostaltxt, "+
+		"RTRIM(LTRIM(cl_tele)) as teletxt, "+
+		"RTRIM(LTRIM(cl_celu)) as celutxt, "+
+		"RTRIM(LTRIM(cl_fax)) as faxtxt, "+
+		"RTRIM(LTRIM(cl_mail)) as mailtxt, "+
+		"RTRIM(LTRIM(cl_gara)) as garantiatxt, "+
+		"RTRIM(LTRIM(cl_gate)) as garantiatelefonotxt, "+
+		"RTRIM(LTRIM(cl_gadi)) as garantiadomiciliotxt, "+
+		"RTRIM(LTRIM(cl_cuenta)) as cuentatxt, "+
+		"RTRIM(LTRIM(cl_nupro)) as nroproveedortxt, "+
+		"RTRIM(LTRIM(cl_empe)) as vendedortxt, "+
+		"CONVERT(varchar(13), cl_fena, 103) as fechaaltaf, "+
+		"CASE WHEN clientes.cl_ctac = 'S' THEN 'Si' WHEN clientes.cl_ctac = 'N' THEN 'No' END as cuentacorrientatxt, "+
+		"CASE WHEN clientes.cl_habi = 'S' THEN 'Si' WHEN clientes.cl_habi = 'N' THEN 'No' END as habilitadotxt "+
+		"from clientes where cl_nume = "+numero, cb);
 }
 
 function sp_clientes(o, cb){
-	conn("sp_clientes "+o.numero+", '"+o.nombre+"', '"+o.cuenta+"'", cb);
+	conn("sp_clientes "+o.numero+", '"+o.razonsocial+"', '"+o.direccion+"', '"+o.localidad+"', '"+o.provincia+"', '"+o.codigopostal+"', "+
+		"'"+o.telefono+"', '"+o.cuit+"', '"+o.categoria+"', '"+o.listadeprecios+"', "+o.credlimite+", '"+o.fecha_alta+"', "+
+		"'"+o.posee+"', '"+o.habilitada+"', '"+o.baja+"', '"+o.saldopositivo+"', '"+o.vendedor+"', '"+o.garantia+"', '"+
+		o.domicilio+"', '"+o.te+"', '"+o.email+"', '"+o.fax+"', "+o.tipodoc+", '"+o.celular+"', '', '"+o.cuentacontable+"', "+
+		"'"+o.nro_proveedor+"', "+o.nrodoc, cb);
 }
 
 function del(numero, cb){
@@ -29,14 +54,7 @@ function getNextNumero(cb){
 	conn("select ISNULL(max(cl_nume), 0)+1 as proximo_codigo from clientes", cb);
 }
 
-function validacionMovimientos(numero, cb){
-	// sp_clientes
-	// @cl_nume Int, @cl_apel char(30), @cl_direc char(40), @cl_local char(20), 
-	// @cl_prov char(15), @cl_cp char(8), @cl_tele char(20), @cl_cuit char(13), @cl_iva smallint ,
-	// @cl_lista char(1), @cl_credito decimal(10), @cl_fena datetime, @cl_ctac char(1), @cl_habi char(1),
-	// @cl_baja char(1), @cl_habi2 char(1), @cl_empe int, @cl_gara char(30), @cl_gadi char(40),
-	// @cl_gate char(20), @cl_mail char(40), @cl_fax char(20), @cl_tido smallint, @cl_celu char(20),
-	// @cl_legajo char(10), @cl_cuenta char(10), @cl_nupro char(10) , @cl_docu decimal(9,0)
+function validacionMovimientos(numero, cb){	
 	conn("select top 1(ps_fecha), ps_coeg from pcas where ps_coeg = "+numero+" "+
 		"union "+
 		"select top 1(ct_fecm), ct_coeg from corri where ct_coeg = "+numero, cb)
